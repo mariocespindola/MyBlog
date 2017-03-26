ee24074b5a6035fff24e0dbacc114b28119360aa
Mario CE <marioc.espindola@gmail.com>
2017-03-25 20:54:59 -0600

    Post read time

--- MyBlog/posts/models.py
+++ MyBlog/posts/models.py
@@ -6,13 +6,15 @@ from django.conf import settings
 from django.utils import timezone
 from django.contrib.contenttypes.models import ContentType
 from django.utils.safestring import mark_safe
-
 from django.utils.text import slugify
-from markdown_deux import markdown
-from MyBlog.comments.models import Comment
 
+from markdown_deux import markdown
+
+from MyBlog.comments.models import Comment
+from .utils import get_read_time
 
 # Create your models here.
+
 
 class PostManager(models.Manager):
     def active(self, *args, **kwargs):
@@ -34,6 +36,7 @@ class Post(models.Model):
     content = models.TextField()
     draft = models.BooleanField(default=False)
     publish = models.DateField(auto_now=False, auto_now_add=False)
+    read_time = models.IntegerField(default=0)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True, null=True)
 
@@ -81,6 +84,11 @@ def create_slug(instance, new_slug=None):
 def pre_save_post_receiver(sender, instance, *args, **kwargs):
     if not instance.slug:
         instance.slug = create_slug(instance)
+
+    if instance.content:
+        html_string = instance.markdown()
+        read_time_var = get_read_time(html_string)
+        instance.read_time = read_time_var
 
 
 pre_save.connect(pre_save_post_receiver, sender=Post)
