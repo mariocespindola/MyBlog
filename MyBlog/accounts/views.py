bd9f2b58ce120e0433cfeb4c7d95ac2a06a89f48
Mario CE <marioc.espindola@gmail.com>
2017-03-25 23:36:51 -0600

    user register form/view

--- MyBlog/accounts/views.py
+++ MyBlog/accounts/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import authenticate, get_user_model, login, logout
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .forms import UserLoginForm
+from .forms import UserLoginForm, UserRegisterForm
 
 
 # Create your views here.
@@ -15,15 +15,31 @@ def login_view(request):
         password = form.cleaned_data.get('password')
         user = authenticate(username=username, password=password)
         login(request, user)
-        print(request.user.is_authenticated())
+        return redirect('/')
 
     return render(request, "form.html", {"form": form, "title": title})
 
 
 def register_view(request):
-    return render(request, 'form.html', {})
+    title = 'Register'
+    form = UserRegisterForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=False)
+        password = form.cleaned_data.get('password')
+        user.set_password(password)
+        user.save()
+        new_user = authenticate(username=user.username, password=password)
+        login(request, user)
+        return redirect('/')
+
+    context = {
+        'form': form,
+        'title': title
+    }
+    return render(request, 'form.html', context)
 
 
 def logout_view(request):
     logout(request)
-    return render(request, 'form.html', {})
+    return redirect('/')
+
