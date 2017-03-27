cc8eb576c19ef880ec172d103c2e49613f00a484
Mario CE <marioc.espindola@gmail.com>
2017-03-27 03:58:36 -0600

    update & delete view api

--- MyBlog/posts/api/urls.py
+++ MyBlog/posts/api/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 
-from .views import PostListAPIView, PostDetailAPIView
+from .views import PostListAPIView, PostDetailAPIView, PostUpdateAPIView, PostDeleteAPIView
 
 urlpatterns = [
     url(r'^$', PostListAPIView.as_view(), name='list'),
-    url(r'^(?P<slug>[w-]+)/$', PostDetailAPIView.as_view(), name='detail')
+    url(r'^(?P<slug>[w-]+)/$', PostDetailAPIView.as_view(), name='detail'),
+    url(r'^(?P<slug>[w-]+)/edit/$', PostUpdateAPIView.as_view(), name='update'),
+    url(r'^(?P<slug>[w-]+)/delete/$', PostDeleteAPIView.as_view(), name='delete'),
+
 ]
--- MyBlog/posts/api/views.py
+++ MyBlog/posts/api/views.py
@@ -1,5 +1,4 @@
-from rest_framework.generics import ListAPIView,RetrieveAPIView
-
+from rest_framework.generics import ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 from MyBlog.posts.models import Post
 from .serializers import PostDetailSerializer, PostListSerializer
 
@@ -10,9 +9,18 @@ class PostDetailAPIView(RetrieveAPIView):
     lookup_field = 'slug'
 
 
+class PostUpdateAPIView(UpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostDetailSerializer
+    lookup_field = 'slug'
+
+
+class PostDeleteAPIView(DestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostDetailSerializer
+    lookup_field = 'slug'
+
+
 class PostListAPIView(ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostListSerializer
-
-
-
